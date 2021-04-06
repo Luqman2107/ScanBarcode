263b13774e47ded8921a3144e8af07de367e874b
Luqman <e41180965@student.polije.ac.id>
2021-04-07 06:28:07 +0700

    aa

---
+++ ScanBarcode/app/src/main/java/com/example/scanbarcode/InputActivity.java
@@ -0,0 +1,270 @@
+package com.example.scanbarcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.scanbarcode.ConfigFile.ServerApi;
+import com.example.scanbarcode.Model.ModelDataAdm;
+import com.example.scanbarcode.Model.ModelInOut;
+import com.example.scanbarcode.Model.ModelLine;
+import com.example.scanbarcode.Model.ModelStation;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputActivity extends AppCompatActivity {
+    Spinner dataAdm, dataLine, dataStation, dataInOut;
+    ImageView scanBarcode;
+    RequestQueue requestQueue;
+
+    List<ModelDataAdm> DataAdmList;
+    List<ModelLine> LineList;
+    List<ModelStation> StationList;
+    List<ModelInOut> InOutList;
+
+    ArrayAdapter<ModelDataAdm> DataAdmAdapter;
+    ArrayAdapter<ModelLine> LineAdapter;
+    ArrayAdapter<ModelStation> StationAdapter;
+    ArrayAdapter<ModelInOut> InOutAdapter;
+
+    String admData, lineData, stationData, inoutData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+        init();
+
+        DataAdmList = new ArrayList<>();
+        DataAdmList.add(new ModelDataAdm("0", "Select Admin"));
+        loadAdm();
+        dataAdm.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ModelDataAdm dataSelected = (ModelDataAdm) adapterView.getSelectedItem();
+                Toast.makeText(InputActivity.this, dataSelected.getSequenceNo() + " " + dataSelected.getAdm_Name(), Toast.LENGTH_SHORT).show();
+                admData = dataSelected.getAdm_Name();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        LineList = new ArrayList<>();
+        LineList.add(new ModelLine("0", "Select Line"));
+        loadLine();
+        dataLine.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ModelLine dataSelected = (ModelLine) adapterView.getSelectedItem();
+                lineData = dataSelected.getLine_No();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        StationList = new ArrayList<>();
+        StationList.add(new ModelStation("0", "Select Station"));
+        loadStation();
+        dataStation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ModelStation dataSelected = (ModelStation) adapterView.getSelectedItem();
+                stationData = dataSelected.getStation_Name();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        InOutList = new ArrayList<>();
+        InOutList.add(new ModelInOut("0", "Select In / Out"));
+        loadInOut();
+        dataInOut.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ModelInOut dataSelected = (ModelInOut) adapterView.getSelectedItem();
+                inoutData = dataSelected.getIO_Name();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        scanBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                if (dataAdm.getSelectedItem().toString().trim().equals("Select Admin")) {
+//                    Toast.makeText(InputActivity.this, "Select Admin !", Toast.LENGTH_SHORT).show();
+//                }
+//                if (dataLine.getSelectedItem().toString().trim().equals("Select Line")){
+//                    Toast.makeText(InputActivity.this, "Select Line !", Toast.LENGTH_SHORT).show();
+//                }
+//                if (dataStation.getSelectedItem().toString().trim().equals("Select Station")) {
+//                    Toast.makeText(InputActivity.this, "Select Station !", Toast.LENGTH_SHORT).show();
+//                }
+//                if (dataInOut.getSelectedItem().toString().trim().equals("Select In / Out")) {
+//                    Toast.makeText(InputActivity.this, "Select In / Out !", Toast.LENGTH_SHORT).show();
+//                }
+
+                Intent input = new Intent(InputActivity.this, MainActivity.class);
+                input.putExtra("intent_admData", admData);
+                input.putExtra("intent_lineData", lineData);
+                input.putExtra("intent_stationData", stationData);
+                input.putExtra("intent_inoutData", inoutData);
+                startActivity(input);
+                finish();
+            }
+        });
+    }
+
+    public void init(){
+        requestQueue = Volley.newRequestQueue(this);
+        dataAdm = findViewById(R.id.adm_name);
+        dataLine = findViewById(R.id.line_no);
+        dataStation = findViewById(R.id.station);
+        dataInOut = findViewById(R.id.in_out);
+        scanBarcode = findViewById(R.id.img_scan);
+    }
+
+    public void loadAdm(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, ServerApi.URL_DATAADM, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray data = jsonObject.getJSONArray("data");
+
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject itemAdm = data.getJSONObject(i);
+
+                        DataAdmList.add(new ModelDataAdm(itemAdm.getString("SequenceNo"), itemAdm.getString("Adm_Name")));
+                    }
+                    DataAdmAdapter = new ArrayAdapter(InputActivity.this, R.layout.simple_spinner_item, DataAdmList);
+                    DataAdmAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+                    dataAdm.setAdapter(DataAdmAdapter);
+                } catch (JSONException e) {
+                    Toast.makeText(InputActivity.this, "Codingan Error !", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(InputActivity.this, "No Data !", Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    public void loadLine(){
+        StringRequest stringRequest2 = new StringRequest(Request.Method.GET, ServerApi.URL_DATALINE, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray data = jsonObject.getJSONArray("data");
+
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject itemLine = data.getJSONObject(i);
+
+                        LineList.add(new ModelLine(itemLine.getString("SequenceNo"), itemLine.getString("Line_No")));
+                    }
+                    LineAdapter = new ArrayAdapter(InputActivity.this, R.layout.simple_spinner_item, LineList);
+                    LineAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+                    dataLine.setAdapter(LineAdapter);
+                } catch (JSONException e) {
+                    Toast.makeText(InputActivity.this, "Codingan Error !", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(InputActivity.this, "No Data !", Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue.add(stringRequest2);
+    }
+
+    public void loadStation(){
+        StringRequest stringRequest3 = new StringRequest(Request.Method.GET, ServerApi.URL_DATASTATION, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray data = jsonObject.getJSONArray("data");
+
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject stationLine = data.getJSONObject(i);
+
+                        StationList.add(new ModelStation(stationLine.getString("SequenceNo"), stationLine.getString("Station_Name")));
+                    }
+                    StationAdapter = new ArrayAdapter(InputActivity.this, R.layout.simple_spinner_item, StationList);
+                    StationAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+                    dataStation.setAdapter(StationAdapter);
+                } catch (JSONException e) {
+                    Toast.makeText(InputActivity.this, "Codingan Error !", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(InputActivity.this, "No Data !", Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue.add(stringRequest3);
+    }
+
+    public void loadInOut(){
+        StringRequest stringRequest4 = new StringRequest(Request.Method.GET, ServerApi.URL_DATAINOUT, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray data = jsonObject.getJSONArray("data");
+
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject inoutLine = data.getJSONObject(i);
+
+                        InOutList.add(new ModelInOut(inoutLine.getString("SequenceNo"), inoutLine.getString("IO_Name")));
+                    }
+                    InOutAdapter = new ArrayAdapter(InputActivity.this, R.layout.simple_spinner_item, InOutList);
+                    InOutAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+                    dataInOut.setAdapter(InOutAdapter);
+                } catch (JSONException e) {
+                    Toast.makeText(InputActivity.this, "Codingan Error !", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(InputActivity.this, "No Data !", Toast.LENGTH_LONG).show();
+            }
+        });
+        requestQueue.add(stringRequest4);
+    }
+}
--- ScanBarcode/app/src/main/java/com/example/scanbarcode/MainActivity.java
+++ ScanBarcode/app/src/main/java/com/example/scanbarcode/MainActivity.java
@@ -3,22 +3,58 @@ package com.example.scanbarcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.scanbarcode.ConfigFile.ServerApi;
 import com.google.zxing.Result;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
     ZXingScannerView mScannerView;
+    String Barcode;
+//    Boolean doubleBackToExit;
+
+    String admData, lineData, stationData, inoutData;
+
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
         mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
 
+//        Parsing data
+        Intent intent = getIntent();
+        admData = intent.getStringExtra("intent_admData");
+        lineData = intent.getStringExtra("intent_lineData");
+        stationData = intent.getStringExtra("intent_stationData");
+        inoutData = intent.getStringExtra("intent_inoutData");
+
+        Log.e("Hasil", admData);
+        Log.e("Hasil", lineData);
+        Log.e("Hasil", stationData);
+        Log.e("Hasil", inoutData);
     }
 
     @Override
@@ -41,10 +77,87 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Scan Result");
         builder.setMessage(rawResult.getText() + " (" + rawResult.getBarcodeFormat().toString() + ')');
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Simpan",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id) {
+                // jika tombol diklik, maka akan menutup activity ini
+                inputData();
+            }
+        });
+        builder.setNegativeButton("Batal",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // jika tombol ini diklik, akan menutup dialog
+                // dan tidak terjadi apa2
+                dialog.cancel();
+            }
+        });
+
+        Barcode = rawResult.getText() + rawResult.getBarcodeFormat().toString();
+        Log.e("barcode", Barcode);
         //builder.setMessage(rawResult.getBarcodeFormat().toString());
         AlertDialog alert1 = builder.create();
         alert1.show();
 
         mScannerView.resumeCameraPreview(this);
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        if (doubleBackToExit) {
+//            Intent abc = new Intent(MainActivity.this, InputActivity.class);
+//            startActivity(abc);
+//            finish();
+//        }
+//
+//        this.doubleBackToExit = true;
+//        Toast.makeText(this, "Tekan sekali lagi untuk kembali", Toast.LENGTH_SHORT).show();
+//
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                doubleBackToExit=false;
+//            }
+//        }, 1000);
+//    }
+
+    public void inputData() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ServerApi.URL_DATASCAN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    Boolean staus = jsonObject.getBoolean("status");
+                    if (staus) {
+                        Toast.makeText(MainActivity.this, jsonObject.getString("pesan"), Toast.LENGTH_SHORT).show();
+                        Intent save = new Intent(MainActivity.this, InputActivity.class);
+                        startActivity(save);
+                        finish();
+                    } else {
+                        Toast.makeText(MainActivity.this, jsonObject.getString("pesan"), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(MainActivity.this, "Codingan Error !", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "No Data !", Toast.LENGTH_LONG).show();
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Adm_Name", admData);
+                params.put("Line_No", lineData);
+                params.put("Station_Name", stationData);
+                params.put("IO_Name", inoutData);
+                params.put("BarcodeNo", Barcode);
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 }
